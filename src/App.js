136368edd86bb721f1e2858c682c723e61700268
Andrew Hulme <andrewhulme1993@gmail.com>
2020-10-14 12:22:15 +0100

    Adds schools and news components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
-// import logo from "./logo.svg";
 import "./App.css";
+import Schools from "./components/schools.jsx";
+import News from "./components/news.jsx";
 
 class App extends Component {
   state = {};
   render() {
-    return <div className="App">Hello World!</div>;
+    return (
+      <div className="App">
+        <Schools />
+        <News />
+      </div>
+    );
   }
 }
 
---
+++ src/components/schools.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Schools() {
+  return (
+    <div>
+      <h1>Schools Page</h1>
+    </div>
+  );
+}
+
+export default Schools;
