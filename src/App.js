7784546c6aab2d96617e3e42a22f0206984b0a2e
Andrew Hulme <andrewhulme1993@gmail.com>
2020-10-14 12:39:56 +0100

    Sets up routing for schools and news without dynamic school id

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React, { Component } from "react";
 import "./App.css";
 import Schools from "./components/schools.jsx";
 import News from "./components/news.jsx";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 class App extends Component {
   state = {};
   render() {
     return (
-      <div className="App">
-        <Schools />
-        <News />
-      </div>
+      <Router>
+        <div className="App">
+          <Route path="/schools" exact component={Schools} />
+          <Route path="/schools/news" component={News} />
+        </div>
+      </Router>
     );
   }
 }
