c348e0328f872e7a418037c395a2cbf0e7b35c45
Andrew Hulme <andrewhulme1993@gmail.com>
2020-10-14 13:18:08 +0100

    Adds a back button from /news

--- src/components/news.jsx
+++ src/components/news.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function News() {
   return (
     <div>
       <h1>News Page</h1>
+      <Link to="/schools"> back >> </Link>
     </div>
   );
 }
