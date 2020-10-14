0a4db7ba1fe90cd4871c0534df497a28376870b7
Andrew Hulme <andrewhulme1993@gmail.com>
2020-10-14 13:48:02 +0100

    Shows list of all schools from backend on /schools page

--- src/components/schools.jsx
+++ src/components/schools.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function Schools() {
+  useEffect(() => {
+    fetchSchools();
+  }, []);
+
+  const [schools, setSchools] = useState([]);
+
+  const fetchSchools = async () => {
+    const data = await fetch("https://api.schoolspider.co.uk/v1/schools");
+
+    const schoolsList = await data.json();
+    console.log(schoolsList.data);
+
+    setSchools(schoolsList.data);
+  };
+
   return (
     <div>
       <h1>Schools Page</h1>
+      {schools.map((school) => (
+        <h3 key={school.id}>{school.title}</h3>
+      ))}
     </div>
   );
 }
