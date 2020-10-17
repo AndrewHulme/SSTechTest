7f385c715b0ef39e56a949a46a101bc8fb422d0e
Andrew Hulme <andrewhulme1993@gmail.com>
2020-10-17 18:34:50 +0100

    Shows primary school name at the top of each newsList page

--- src/components/newsList.jsx
+++ src/components/newsList.jsx
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-function NewsList({ match }, props) {
+function NewsList(props) {
   useEffect(() => {
     fetchNews();
-    console.log(match);
     console.log(props);
   }, []);
 
@@ -13,7 +12,7 @@ function NewsList({ match }, props) {
 
   const fetchNews = async () => {
     const data = await fetch(
-      `https://api.schoolspider.co.uk/v1/schools/${match.params.id}/news`
+      `https://api.schoolspider.co.uk/v1/schools/${props.match.params.id}/news`
     );
     const newsList = await data.json();
 
@@ -24,13 +23,14 @@ function NewsList({ match }, props) {
   return (
     <div>
       <h1>News Page</h1>
+      <h1 className="text-2xl font-bold">{props.location.state.schoolName}</h1>
       <Link to="/schools"> back >> </Link>
 
       <div className="grid grid-cols-3 gap-4">
         {news.map((article) => (
           <div key={article.id} className="p-4 m-4 bg-gray-200 rounded">
             <h3>{article.title}</h3>
-            <Link to={`/schools/${match.params.id}/news/${article.id}`}>
+            <Link to={`/schools/${props.match.params.id}/news/${article.id}`}>
               <button className="bg-green-600 hover:bg-blue-700 text-white py-2 px-4 rounded">
                 view >
               </button>
