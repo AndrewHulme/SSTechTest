0df46504c787d9150ff55609010c8daf4a44a544
Andrew Hulme <andrewhulme1993@gmail.com>
2020-10-19 19:30:58 +0100

    Extracts Card component

---
+++ src/components/card.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import ViewButton from "./viewButton.jsx";
+
+function Card(props) {
+  return (
+    <div key={props.result.id} className="p-4 m-4 bg-gray-200 rounded">
+      <h3>{props.result.title}</h3>
+
+      {props.page === "schoolsList" ? (
+        <div>
+          <Link to={`/schools/${props.result.id}/news`}>
+            <ViewButton />
+          </Link>{" "}
+        </div>
+      ) : (
+        <div>
+          <Link to={`/schools/${props.schoolId}/news/${props.result.id}`}>
+            <ViewButton />
+          </Link>{" "}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Card;
--- src/components/cardGrid.jsx
+++ src/components/cardGrid.jsx
@@ -1,36 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import ViewButton from "./viewButton.jsx";
+// import { Link } from "react-router-dom";
+// import ViewButton from "./viewButton.jsx";
+import Card from "./card.jsx";
 
 function CardGrid(props) {
   return (
     <div className="grid grid-cols-3 gap-4">
       {props.filteredSearch.map((result) => (
-        <div key={result.id} className="p-4 m-4 bg-gray-200 rounded">
-          <h3>{result.title}</h3>
-
-          {props.page === "schoolsList" ? (
-            <div>
-              <Link
-                to={{
-                  pathname: `/schools/${result.id}/news`,
-                }}
-              >
-                <ViewButton />
-              </Link>{" "}
-            </div>
-          ) : (
-            <div>
-              <Link
-                to={{
-                  pathname: `/schools/${props.schoolId}/news/${result.id}`,
-                }}
-              >
-                <ViewButton />
-              </Link>{" "}
-            </div>
-          )}
-        </div>
+        <Card
+          result={result}
+          filteredSearch={props.filteredSearch}
+          page={props.page}
+          schoolId={props.schoolId}
+        />
       ))}
     </div>
   );
