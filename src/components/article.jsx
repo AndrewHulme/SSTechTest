cdaeb89dbcdb65f5877fdcf0f2c3e8c1864c27a8
Andrew Hulme <andrewhulme1993@gmail.com>
2020-10-20 11:59:31 +0100

    Refactors article component

--- src/components/article.jsx
+++ src/components/article.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 import Lightbox from "react-image-lightbox";
 import SchoolTitle from "./schoolTitle.jsx";
 import BackButton from "./backButton.jsx";
