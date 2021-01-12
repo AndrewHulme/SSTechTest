c8430bbe2e3aba38c8421a02c8e475657ddd0b69
Andrew Hulme <andrewhulme1993@gmail.com>
2021-01-12 16:16:29 +0000

    Adds future object for Tailwind CSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   plugins: [require("tailwindcss"), require("autoprefixer")],
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+    defaultLineHeights: true,
+    standardFontWeights: true,
+  },
 };
