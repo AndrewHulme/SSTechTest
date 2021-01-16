d602dde98d736cae3eaeadc688f7f4bc5a335777
Andrew Hulme <andrewhulme1993@gmail.com>
2021-01-16 21:55:59 +0000

    Adds Origami teaser for displaying cards

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -4,18 +4,42 @@ import ViewButton from "./viewButton.jsx";
 
 function Card(props) {
   return (
-    <div key={props.result.id} className="p-4 m-4 bg-gray-200 rounded">
-      <h3>{props.result.title}</h3>
+    <div
+      class="o-teaser o-teaser--small   "
+      data-o-component="o-teaser"
+      key={props.result.id}
+    >
+      <div class="o-teaser__content">
+        {/* <div class="o-teaser__meta">
+          <a href="#" class="o-teaser__tag">
+            World
+          </a>
+        </div> */}
 
-      {props.page === "schoolsList" ? (
-        <Link to={`/schools/${props.result.id}/news`}>
-          <ViewButton />
-        </Link>
-      ) : (
-        <Link to={`/schools/${props.schoolId}/news/${props.result.id}`}>
-          <ViewButton />
-        </Link>
-      )}
+        <h2 class="o-teaser__heading">
+          <a>{props.result.title}</a>
+        </h2>
+
+        {props.page === "schoolsList" ? (
+          <Link to={`/schools/${props.result.id}/news`}>
+            <ViewButton />
+          </Link>
+        ) : (
+          <Link to={`/schools/${props.schoolId}/news/${props.result.id}`}>
+            <ViewButton />
+          </Link>
+        )}
+
+        {/* <div class="o-teaser__timestamp">
+          <time
+            data-o-component="o-date"
+            class="o-teaser__timestamp-date"
+            datetime="2016-02-29T12:35:48Z"
+          >
+            2016-02-29T12:35:48Z
+          </time>
+        </div> */}
+      </div>
     </div>
   );
 }
