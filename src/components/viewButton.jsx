20a5dea250faa673da078f5a4d281477f3f39d21
Andrew Hulme <andrewhulme1993@gmail.com>
2021-01-16 12:44:13 +0000

    Adds FT Origami components with navbar and buttons

--- src/components/viewButton.jsx
+++ src/components/viewButton.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 function ViewButton() {
-  return (
-    <button className="bg-green-600 hover:bg-blue-700 text-white py-2 px-4 rounded">
-      view >
-    </button>
-  );
+  return <button className="o-buttons o-buttons--primary">View</button>;
 }
 
 export default ViewButton;
