e2a7f76523ab79d6c6063e441ebd6fa6f0549cff
Andrew Hulme <andrewhulme1993@gmail.com>
2021-01-18 11:35:17 +0000

    Changes display of video grid to use FT Origami components

--- src/components/videoGrid.jsx
+++ src/components/videoGrid.jsx
@@ -2,20 +2,28 @@ import React from "react";
 
 function VideoGrid(props) {
   return (
-    <div className="grid grid-cols-2 gap-3 m-4">
-      {props.videos.map((video, index) => (
-        <div key={index}>
-          {video.content.includes("<iframe") ? (
-            <div className="videoWrapper">
-              <div dangerouslySetInnerHTML={{ __html: video.content }}></div>
+    <div className="o-grid-container">
+      <div className="o-grid-row">
+        {props.videos.map((video, index) => (
+          <div key={index}>
+            <div data-o-grid-colspan="12">
+              <div className="demo-cell">
+                {video.content.includes("<iframe") ? (
+                  <div className="videoWrapper">
+                    <div
+                      dangerouslySetInnerHTML={{ __html: video.content }}
+                    ></div>
+                  </div>
+                ) : (
+                  <video width="300" controls>
+                    <source src={video.content} type="video/mp4"></source>
+                  </video>
+                )}
+              </div>
             </div>
-          ) : (
-            <video width="300" controls>
-              <source src={video.content} type="video/mp4"></source>
-            </video>
-          )}
-        </div>
-      ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
