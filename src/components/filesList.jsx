da4651102c6f5d0de125b62582dc1a55ac2ed735
Andrew Hulme <andrewhulme1993@gmail.com>
2021-01-18 12:34:38 +0000

    Changes display for filesList to use FT Origami components

--- src/components/filesList.jsx
+++ src/components/filesList.jsx
@@ -1,19 +1,28 @@
 import React from "react";
-import paperclip from "../images/paperclip.png";
 
 function FilesList(props) {
   return (
-    <div className="m-4">
-      {props.files.map((file, index) => (
-        <a
-          key={index}
-          href={file.file}
-          className="flex items-center rounded overflow-hidden shadow-lg px-6 py-4"
-        >
-          <img src={paperclip} alt="Paperclip" className="w-10 h-10 mr-4" />
-          <div className="text-gray-600">{file.title}.pdf</div>
-        </a>
-      ))}
+    <div className="o-grid-container">
+      <div className="o-grid-row">
+        {props.files.map((file, index) => (
+          <div data-o-grid-colspan="12">
+            <div className="demo-cell">
+              <div
+                className="o-teaser o-teaser--small   "
+                data-o-component="o-teaser"
+              >
+                <div className="o-teaser__content">
+                  <h2 className="o-teaser__heading fileHeading">
+                    <a key={index} href={file.file}>
+                      <div>{file.title}.pdf</div>
+                    </a>
+                  </h2>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
