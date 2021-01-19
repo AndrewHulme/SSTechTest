8b1908cdb89f3965dfc53be62338c362d3647c3c
Andrew Hulme <andrewhulme1993@gmail.com>
2021-01-19 21:40:34 +0000

    Adds Cypress test to show article headline

--- cypress/integration/article.spec.js
+++ cypress/integration/article.spec.js
@@ -1,17 +1,22 @@
 describe("Article", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000");
+    cy.get("#427").click();
+    cy.get("#57537").click();
+  });
+
+  it("should direct to the correct URL", () => {
+    cy.url().should("eq", "http://localhost:3000/schools/427/news/57537");
   });
 
   it("should show the school name", () => {
-    cy.url().should("eq", "http://localhost:3000/schools");
-
-    cy.get("#427").click();
-    cy.url().should("eq", "http://localhost:3000/schools/427/news");
-
-    cy.get("#57537").click();
-    cy.url().should("eq", "http://localhost:3000/schools/427/news/57537");
-
     cy.get("#schoolTitle").should("have.text", "Ash Croft Primary Academy");
+  });
+
+  it("should show the article headline", () => {
+    cy.get("#articleTitle").should(
+      "have.text",
+      "Letter to all parents - New National Restrictions"
+    );
   });
 });
--- src/components/articleText.jsx
+++ src/components/articleText.jsx
@@ -4,7 +4,9 @@ function ArticleText(props) {
   return (
     <div class="o-topper o-topper--centered o-topper--color-paper">
       <div class="o-topper__content">
-        <h1 class="o-topper__headline">{props.article.title}</h1>
+        <h1 id="articleTitle" class="o-topper__headline">
+          {props.article.title}
+        </h1>
         <div class="o-topper__standfirst">
           <div
             className="bodyText"
