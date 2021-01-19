0fc3362907cfab4dc91eebe23dad4084783e3061
Andrew Hulme <andrewhulme1993@gmail.com>
2021-01-19 13:03:14 +0000

    Adds test for schoolsList for displaying search bar

--- src/components/schoolsList.test.js
+++ src/components/schoolsList.test.js
@@ -4,35 +4,35 @@ import SchoolsList from "./schoolsList";
 import SearchBar from "./searchBar";
 import CardGrid from "./cardGrid";
 
+const schools = [
+  {
+    active: true,
+    address: "Bolton Road Abbey Village Chorley PR6 8DD",
+    domain: "www.abbeyvillage.schoolspider.co.uk",
+    id: 486,
+    title: "Abbey Village Primary School",
+    using_wonde: true,
+  },
+  {
+    active: true,
+    address: "Livesey Street Manchester Manchester M40 7PR",
+    domain: "www.abbott.manchester.sch.uk",
+    id: 228,
+    title: "Abbott Community Primary School",
+    using_wonde: true,
+  },
+  {
+    active: true,
+    address: "Abingdon Road Reddish Stockport SK5 7ET",
+    domain: "www.abingdon.schoolspider.co.uk",
+    id: 527,
+    title: "Abingdon Primary School",
+    using_wonde: true,
+  },
+];
+
 describe("SchoolsList", () => {
   it("should only render CardGrid initially", () => {
-    const schools = [
-      {
-        active: true,
-        address: "Bolton Road Abbey Village Chorley PR6 8DD",
-        domain: "www.abbeyvillage.schoolspider.co.uk",
-        id: 486,
-        title: "Abbey Village Primary School",
-        using_wonde: true,
-      },
-      {
-        active: true,
-        address: "Livesey Street Manchester Manchester M40 7PR",
-        domain: "www.abbott.manchester.sch.uk",
-        id: 228,
-        title: "Abbott Community Primary School",
-        using_wonde: true,
-      },
-      {
-        active: true,
-        address: "Abingdon Road Reddish Stockport SK5 7ET",
-        domain: "www.abingdon.schoolspider.co.uk",
-        id: 527,
-        title: "Abingdon Primary School",
-        using_wonde: true,
-      },
-    ];
-
     const wrapper = shallow(
       <SchoolsList schools={schools} showSearchBar={false} />
     );
@@ -40,5 +40,15 @@ describe("SchoolsList", () => {
     const searchBar = wrapper.find(SearchBar);
     expect(cardGrid.exists()).toBe(true);
     expect(searchBar.exists()).toBe(false);
+  });
+
+  it("should render both CardGrid and SearchBar when showSearchBar=true", () => {
+    const wrapper = shallow(
+      <SchoolsList schools={schools} showSearchBar={true} />
+    );
+    const cardGrid = wrapper.find(CardGrid);
+    const searchBar = wrapper.find(SearchBar);
+    expect(cardGrid.exists()).toBe(true);
+    expect(searchBar.exists()).toBe(true);
   });
 });
