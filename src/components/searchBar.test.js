9561ee1bb023af4d78d1f312c38e653c54553b75
Andrew Hulme <andrewhulme1993@gmail.com>
2021-01-19 11:33:41 +0000

    Adds test for searchBar for the input value

--- src/components/searchBar.test.js
+++ src/components/searchBar.test.js
@@ -3,19 +3,23 @@ import { shallow } from "enzyme";
 import SearchBar from "./searchBar";
 
 describe("SearchBar", () => {
-  it("should call updateSearch", () => {
+  it("should call updateSearch onChange", () => {
     const updateSearch = jest.fn();
     const initialProps = { updateSearch };
 
     const wrapper = shallow(<SearchBar {...initialProps} />);
     const input = wrapper.find("input");
 
-    const event = {
-      target: { value: "hello" },
-    };
-
+    const event = { target: { value: "hello" } };
     input.simulate("change", event);
 
     expect(updateSearch).toHaveBeenCalledWith("hello");
+  });
+
+  it("should have a value of props.search", () => {
+    const wrapper = shallow(<SearchBar search="testing" />);
+    const input = wrapper.find("input");
+
+    expect(input.props().value).toEqual("testing");
   });
 });
